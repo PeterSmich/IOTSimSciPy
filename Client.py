66fb3fd443251c86606cb46d0874f60d4cc99c05
PeterSmich <kovachpeter@gmail.com>
2017-04-03 18:00:22 +0200

    Peti

--- Client.py
+++ Client.py
@@ -15,32 +15,43 @@ class Client(tk.Frame):
 
         master.bind("<<RefreshMap>>", self.refresh_map)
         #BluePrint and contorl panels
-        self.f1 = tk.Frame(self)
-        self.f2 = tk.Frame(self, height = 443)
+        self.configure(background='dark slate gray')
+        self.f1 = tk.Frame(self, bg = 'dark slate gray', bd = 5)
+        f2 = tk.Frame(self, bg = 'dark slate gray', bd = 1, pady = 1)
+        self.f2 = tk.Frame(f2, bg = 'gray16', bd = 1, pady = 1)
         #Set blueprint image
         self.bckg_img = tk.PhotoImage(file="blueprint.gif")
-        self.canvas = tk.Canvas(self.f1, width=390, height=440, bd = 3, bg = 'black')
+        self.canvas = tk.Canvas(self.f1, width=390, height=440, bd = 1, bg = 'black')
         self.draw_BP(self.canvas)
 
-        self.f1.grid(row = 0, column = 0)
+        self.f1.pack(side = 'left')
         #Set controls options eg. filters
         self.types_filter = {'glass' : tk.BooleanVar(), 'key' : tk.BooleanVar(), 'phone' : tk.BooleanVar(), 'pen' : tk.BooleanVar()}
-        self.check_Glass = tk.Checkbutton(self.f2, text = 'Glasses', variable = self.types_filter['glass'], bg = self.types['glass'], width = 20, command = self.set_changes)
-        self.check_Key = tk.Checkbutton(self.f2, text = 'Keys', variable = self.types_filter['key'], bg = self.types['key'], width = 20, command = self.set_changes)
-        self.check_Phone = tk.Checkbutton(self.f2, text = 'Phones', variable = self.types_filter['phone'], bg = self.types['phone'], width = 20, command = self.set_changes)
-        self.check_Pen = tk.Checkbutton(self.f2, text = 'Pens', variable = self.types_filter['pen'], bg = self.types['pen'], width = 20, command = self.set_changes)
+        self.check_Glass = tk.Checkbutton(self.f2, text = 'Glasses', variable = self.types_filter['glass'], bg = self.types['glass'],\
+            width = 10, command = self.set_changes, anchor = 'w')
+        self.check_Key = tk.Checkbutton(self.f2, text = 'Keys', variable = self.types_filter['key'], bg = self.types['key'],\
+            width = 10, command = self.set_changes, anchor = 'w')
+        self.check_Phone = tk.Checkbutton(self.f2, text = 'Phones', variable = self.types_filter['phone'], bg = self.types['phone'],\
+            width = 10, command = self.set_changes, anchor = 'w')
+        self.check_Pen = tk.Checkbutton(self.f2, text = 'Pens', variable = self.types_filter['pen'], bg = self.types['pen'],\
+            width = 10, command = self.set_changes, anchor = 'w')
         
         self.check_Glass.select()
         self.check_Key.select()
         self.check_Phone.select()
         self.check_Pen.select()
 
-        self.check_Glass.pack()
-        self.check_Key.pack()
-        self.check_Phone.pack()
-        self.check_Pen.pack()
+        tk.Label(self.f2, text = 'Filter:', bg = 'gray16', fg = 'white', anchor = 'w').pack(fill = 'x')
 
-        self.f2.grid(row = 0, column = 1, sticky = 'n')
+        self.check_Glass.pack(padx = 2, pady = 1)
+        self.check_Key.pack(padx = 2, pady = 1)
+        self.check_Phone.pack(padx = 2, pady = 1)
+        self.check_Pen.pack(padx = 2, pady = 1)
+
+        tk.Label(f2, text = 'IoT indoor positioning system for disabled people', \
+            bg = 'dark slate gray', fg = 'white', anchor = 'w', wraplength = 100, bd = 5).grid(row = 0, column = 0)
+        self.f2.grid(row = 1, column = 0)
+        f2.pack(padx = 5)
         self.pack()
         #Chack for database connection
         t = threading.Thread(target = self.connect)
