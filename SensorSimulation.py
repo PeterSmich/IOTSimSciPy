e60ff1c5f5bef696d18f017efa4ccdb7f26ea190
khbence <horvathb94@gmail.com>
2017-05-05 16:49:12 +0200

    Dolgozom

--- SensorSimulation.py
+++ SensorSimulation.py
@@ -1,11 +1,24 @@
 import tkinter as tk
+from tkinter import messagebox
 
 class Obj:
 	"""docstring for Obj"""
-	poz_x = 0
-	poz_y = 0
-	id = 0
-	def __init__(self, x, y, id):
+	poz_x = -1
+	poz_y = -1
+	name = "Kecske"
+	circle = None
+	button = None
+	colour = 'Red'
+	def __init__(self, x, y, name, circle, button, color):
+		print("kecske")
+
+	def __init__(self, name, i):
+		self.name = name
+		self.colour = self.get_Colour(i)
+
+	def get_Colour(self, i):
+		colours = {0 : 'Red', 1 : 'Yellow', 2 : 'Green', 3 : 'Blue'}
+		return colours[i]
 
 class Sensor:
 	"""docstring for Sensor"""
@@ -29,13 +42,15 @@ class App:
 	ip = 0
 	port = 0
 	sensors = []
-	objects = []
 
 	def __init__(self):
+		print("kecske")
 
 	def add_object():
+		print("kecske")
 
 	def connect():
+		print("kecske")
 
 	def send():
 		for s in sensors:
@@ -43,25 +58,67 @@ class App:
 
 		send_to()
 	def send_to(json):
+		print("kecske")
 
 	def disconnect():
+		print("kecske")
 
 
 class Window(tk.Frame):
-	obj_type = ['Phone', 'Remooter', 'Mug', 'Key']
-	buttons = []
+	objects_button = []
+	objects = []
+	types = ('Glass', 'Key', 'Phone', 'Pen')
+	selected_object = None;
+
 	app = App()
+
 	def __init__(self, master=None):
 		super().__init__(master)
-		f1 = tk.Frame(self)
-		f2 = tk.Frame(self, )
-		for ot in self.obj_type:
-			self.buttons.append(tk.Button(f1, text = ot))
-		for i in range(1,len(self.buttons)):
-			self.buttons[i].grid(row = i, column = 0, padx = 2, pady = 2)
-		f1.grid(row = 0, column = 1, padx = 2, pady = 2)
-		f2.grid(row = 0, column = 0, padx = 2, pady = 2)
+
+		for i in range(0,4):
+			self.objects.append(Obj(self.types[i], i))
+		#BluePrint and control panels
+		self.configure(background='dark slate gray')
+		self.f1 = tk.Frame(self, bg = 'dark slate gray', bd = 5)
+		f2 = tk.Frame(self, bg = 'dark slate gray', bd = 1, pady = 1)
+		self.f2 = tk.Frame(f2, bg = 'gray16', bd = 1, pady = 1)
+		
+		#Set blueprint image
+		self.bckg_img = tk.PhotoImage(file="blueprint.gif")
+		self.canvas = tk.Canvas(self.f1, width=390, height=440, bd = 1, bg = 'black')
+		self.draw_BP(self.canvas)
+		self.f1.pack(side = 'left')
+
+		self.canvas.bind("<Button-1>", self.draw_Circle)
+		
+		#Set controls options eg. filters
+		tk.Label(self.f2, text = 'Filter:', bg = 'gray16', fg = 'white', anchor = 'w').pack(fill = 'x')
+		for i in range(0,4):
+			self.objects[i].button = tk.Button(self.f2, text = self.objects[i].name, \
+				bg = self.objects[i].colour, width = 10, command = self.select_Object(i))
+			self.objects[i].button.pack(padx = 2, pady = 0)
+		
+
+		tk.Label(f2, text = 'IoT indoor positioning system for disabled people', \
+			bg = 'dark slate gray', fg = 'white', anchor = 'w', wraplength = 100, bd = 5).grid(row = 0, column = 0)
+		self.f2.grid(row = 1, column = 0)
+		f2.pack(padx = 5)
 		self.pack()
+
+	def draw_BP(self,canv):
+		canv.create_image(198, 224, image=self.bckg_img)
+		canv.pack(fill = 'both')
+
+	def set_changes(self):
+		self.changes = True
+
+	def select_Object(self, i):
+		self.selected_object = i
+		print(self.selected_object)
+
+	def draw_Circle(self,event):
+		self.objects[self.selected_object].circle = self.canvas.create_oval(event.x-5, event.y-5, event.x+5, event.y+5, \
+			fill = self.objects[self.selected_object].colour)
 		
 
 		
